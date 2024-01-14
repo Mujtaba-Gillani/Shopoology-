3a96662cac17ee795d9591e43381e8daa7aa14fe
Mujtaba-Gillani <141050502+Mujtaba-Gillani@users.noreply.github.com>
2024-01-14 19:20:44 +0500

    front pages updated and completed.. only js and images files remaining

--- ecommerce/frontend/urls.py
+++ ecommerce/frontend/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.about_us, name='home'),
     path('aboutus/', views.about_us, name='aboutus'),
+    path('menu/', views.menu, name='menu'),
+    path('team/', views.team, name='team'),
+    path('services/', views.service, name='services'),
+    path('testimonial/', views.testimonial, name='testimonial'),
+    path('contactus/', views.contactUs, name='contactus'),
 
 ]
--- ecommerce/frontend/views.py
+++ ecommerce/frontend/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'frontend/home.html')
 
 def about_us(request):
     return render(request, 'frontend/about.html')
+
+
+
+def menu(request):
+    return render(request, 'frontend/menu.html')
+
+def team(request):
+    return render(request, 'frontend/team.html')
+
+def service(request):
+    return render(request, 'frontend/services.html')
+
+def testimonial(request):
+    return render(request, 'frontend/testimonial.html')
+
+def contactUs(request):
+    return render(request, 'frontend/contact.html')
