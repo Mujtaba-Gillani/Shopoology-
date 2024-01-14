2bfba8348774a20704e5259dfab3a7defd9dc0aa
Mujtaba-Gillani <141050502+Mujtaba-Gillani@users.noreply.github.com>
2024-01-14 22:25:13 +0500

    ui updates

--- ecommerce/frontend/urls.py
+++ ecommerce/frontend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.about_us, name='home'),
+    path('', views.home, name='home'),
     path('aboutus/', views.about_us, name='aboutus'),
     path('menu/', views.menu, name='menu'),
     path('team/', views.team, name='team'),
